5e8fb1752c74fdac58adba8e3176124721cba82e
MaDZeRoPiE <sanich2000qq@gmail.com>
2019-09-16 17:35:28 +0300

    some water

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -9,29 +9,40 @@
 
 TBitField::TBitField(int len)
 {
+	BitLen = len;
+	MemLen = (len - 1) / sizeof(TELEM) + 1;
+	pMem = new TELEM(MemLen);
+
 }
 
 TBitField::TBitField(const TBitField &bf) // конструктор копирования
 {
+	BitLen = bf.BitLen;
+	MemLen = bf.MemLen;
+	pMem = new TELEM(MemLen);
+	for (int i = 0; i < MemLen; ++i) *(pMem+i) = *(bf.pMem+i);
 }
 
 TBitField::~TBitField()
 {
+//	delete[] pMem;
 }
 
 int TBitField::GetMemIndex(const int n) const // индекс Мем для бита n
 {
+	return 0;
 }
 
 TELEM TBitField::GetMemMask(const int n) const // битовая маска для бита n
 {
+	return 0;
 }
 
 // доступ к битам битового поля
 
 int TBitField::GetLength(void) const // получить длину (к-во битов)
 {
-  return 0;
+  return BitLen;
 }
 
 void TBitField::SetBit(const int n) // установить бит
@@ -51,6 +62,7 @@ int TBitField::GetBit(const int n) const // получить значение б
 
 TBitField& TBitField::operator=(const TBitField &bf) // присваивание
 {
+	return *this;
 }
 
 int TBitField::operator==(const TBitField &bf) const // сравнение
@@ -65,22 +77,30 @@ int TBitField::operator!=(const TBitField &bf) const // сравнение
 
 TBitField TBitField::operator|(const TBitField &bf) // операция "или"
 {
+	TBitField res(*this);
+	return res;
 }
 
 TBitField TBitField::operator&(const TBitField &bf) // операция "и"
 {
+	TBitField res(*this);
+	return res;
 }
 
 TBitField TBitField::operator~(void) // отрицание
 {
+	TBitField res(*this);
+	return res;
 }
 
 // ввод/вывод
 
 istream &operator>>(istream &istr, TBitField &bf) // ввод
 {
+	return istr;
 }
 
 ostream &operator<<(ostream &ostr, const TBitField &bf) // вывод
 {
+	return ostr;
 }
